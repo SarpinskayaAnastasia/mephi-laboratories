41b564e115766bd520980e0f23105c8b357fe4cf
SarpinskayaAnastasia <74780246+SarpinskayaAnastasia@users.noreply.github.com>
2025-03-01 17:18:11 +0300

    Add files via upload

---
+++ 2 semester/lab2/thrd.py
@@ -0,0 +1,53 @@
+from frst import OnceNode, OneConnected
+
+
+def get_middle(top: OnceNode) -> OnceNode:
+    slow_cursor = top
+    fast_cursor = top
+    while fast_cursor is not None and fast_cursor.next is not None:
+        slow_cursor = slow_cursor.next
+        fast_cursor = fast_cursor.next.next
+    return slow_cursor.next if fast_cursor else slow_cursor  # когда в списке четное количество узлов, быстрый курсор
+    # становится None, а когда нечетное - это все еще узел.
+
+
+def reverse(top: OnceNode) -> OnceNode:
+    prev = None  # это будет конец изначального списка и начало нового, перевернутого
+    curr = top
+    while curr is not None:
+        nxt = curr.next
+        curr.set_next(prev)
+        prev = curr
+        curr = nxt
+    return prev
+
+
+def is_palindrome(left: OnceNode) -> bool:
+    if left.next is None:
+        return True  # ну это если мы единственный символ считаем палиндромом, разумеется
+    scnd_half = get_middle(left)
+    right = reverse(scnd_half)
+    while right is not None:
+        if right != left:  # это стало возможным, потому что в классе узла мы прописали сравнения
+            return False
+        right = right.next
+        left = left.next
+    return True
+
+
+def gen_case(string: str) -> tuple[OnceNode, bool]:
+    string = string.replace(' ', '').lower()
+    temp = OneConnected()
+    for n in string:
+        temp.push_top(n)
+    return temp.top, string == string[::-1]
+
+
+if __name__ == "__main__":
+    cases = []
+    for w in ['12321', '123321', '1', '11', 'А роза упала на лапу Азора', 'gegwg', 'some word...']:
+        cases.append(gen_case(w))
+    for input_top, verdict in cases:
+        assert (is_palindrome(input_top) == verdict), f"Case failed for {input_top}, expected {str(verdict)}"
+    print("All unit tests passed")
+
