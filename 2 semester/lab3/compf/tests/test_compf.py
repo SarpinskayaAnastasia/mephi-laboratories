1d3f91af79bb77c22560e790d4fec58f05eba370
SarpinskayaAnastasia <74780246+SarpinskayaAnastasia@users.noreply.github.com>
2025-03-08 19:22:11 +0300

    Update test_compf.py

--- 2 semester/lab3/compf/tests/test_compf.py
+++ 2 semester/lab3/compf/tests/test_compf.py
@@ -1,5 +1,4 @@
-from compf import Compf
-
+from compf_new import Compf
 
 class TestCompf:
 
@@ -26,7 +25,7 @@ class TestCompf:
 
     # Тесты на порядок выполнения операций
     def test_operations_order1(self):
-        assert self.c.compile("a+c*b") == "a c b * +"
+        assert self.c.compile("a+c*b") == "a c + b *"
 
     def test_operations_order2(self):
         assert self.c.compile("a*b/c") == "a b * c /"
@@ -49,27 +48,27 @@ class TestCompf:
 
     # Тесты на более сложные выражения
     def test_expressions1(self):
-        assert self.c.compile("(a+b)*c+(d-e)/f") == "a b + c * d e - f / +"
+        assert self.c.compile("(a+b)*c+(d-e)/f") == "a b + c d e - + * f /"
 
     def test_expressions2(self):
         assert self.c.compile("c*(c+c+c+c/(c-c-c-c))") == \
-            "c c c + c + c c c - c - c - / + *"
+            "c c c + c + c + c c - c - c - / *"
 
     def test_expressions3(self):
         assert self.c.compile("a/b*c+d*e/(f+g)") == \
-            "a b / c * d e * f g + / +"
+            "a b / c d + * e * f g + /"
 
     def test_expressions4(self):
         assert self.c.compile("a/b*(c+d*e)/(f+g)") == \
-            "a b / c d e * + * f g + /"
+            "a b / c d + e * * f g + /"
 
     def test_expressions5(self):
         assert self.c.compile("a+b*(c-d)*(c+(d-e)/a)/a") == \
-            "a b c d - * c d e - a / + * a / +"
+            "a b + c d - * c d e - + a / * a /"
 
     def test_expressions6(self):
         assert self.c.compile("((c+(c*(c+(c+c/c)))))/(c+c)") == \
-            "c c c c c c / + + * + c c + /"
+            "c c c c c + c / + * + c c + /"
 
     def test_expressions7(self):
         assert self.c.compile("c+(c+(c*(c+(c/(c*(c+c))))))") == \
