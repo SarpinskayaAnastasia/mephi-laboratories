c5e367dd1f1b1a81e7a307e27ab79a03c2cfb237
SarpinskayaAnastasia <74780246+SarpinskayaAnastasia@users.noreply.github.com>
2025-03-31 16:59:01 +0300

    fixes in _1.py

--- 2 semester/lab5/_2.py
+++ 2 semester/lab5/_2.py
@@ -1,25 +1,38 @@
-from _1 import cycle_bin_srch
+import sys
 
+sys.setrecursionlimit(100000)
 
-'''CHATGPT ТУПОЙ УБЛЮДОК ОН НЕ УМЕЕТ КОДИТЬ МРАЗЬ ТУПОРЫЛАЯ
+'''
+CHATGPT ТУПОЙ УБЛЮДОК ОН НЕ УМЕЕТ КОДИТЬ МРАЗЬ ТУПОРЫЛАЯ
 СЕЙЧАС Я НАПИШУ ВЕСЬ КОД И ТОГДА ВОСТОРЖЕСТВУЕТ НАТУРАЛЬНЫЙ ИНТЕЛЛЕКТ
 ДА ЗДРАВСТВУЕТ НАТУРАЛЬНЫЙ ИНТЕЛЛЕКТ
 '''
 
 
+def bin_search(arr, key, low, high):  # В этом есть необходимость, поскольку если функция вдруг выбросит -1,
+    # ничего не получится, а low == 0 нам не подойдет
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] < key:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return low
+
+
 def bin_paste_sort(arr: list):
-    for i in range(1, len(arr)):
+    n = len(arr)
+    for i in range(1, n):
+        j = i - 1
         key = arr[i]
-        position = cycle_bin_srch(arr[0:i], key)
+        k = bin_search(arr, key, 0, j)
 
-        if position == -1:
-            position = 0
+        # Сдвигаем элементы вправо
+        while j >= k:
+            arr[j + 1] = arr[j]
+            j -= 1
 
-        shifting = arr[i]
-        for j in range(i, position):
-            arr[j] = arr[j - 1]
-        arr[position] = shifting
-    return arr
+        arr[k] = key  # Вставляем элемент на нужное место
 
 
 def insertion_sort(arr: list):  # первоначальная функция
@@ -36,5 +49,5 @@ def insertion_sort(arr: list):  # первоначальная функция
 if __name__ == "__main__":
     arr = [3, 4, 5, 6, 3, 3, 2, 53443, 23, 436, 7, 56, 24, 24, 24, 7, 624, 534, 542422, 3, 4, 35, 63, 2]
     print(f'before: {arr}')
-    print(f'after: {bin_paste_sort(arr)}')
-    print(f'after2: {insertion_sort(arr)}')
+    bin_paste_sort(arr)
+    print(f'after: {arr}')
