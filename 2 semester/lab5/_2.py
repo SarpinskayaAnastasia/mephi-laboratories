7460e2e277dc5d0ced2e9c78f4fe982d71e5fcc1
Lizavetick <liza.log2019@bk.ru>
2025-03-31 13:37:48 +0300

    Update _2.py

--- 2 semester/lab5/_2.py
+++ 2 semester/lab5/_2.py
@@ -1,2 +1,10 @@
-def sort_past(arr: list):
-    pass
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
