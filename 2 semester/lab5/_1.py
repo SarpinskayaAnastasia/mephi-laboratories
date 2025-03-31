bc373205f500800c7f7813586cd0e26ca376b246
SarpinskayaAnastasia <74780246+SarpinskayaAnastasia@users.noreply.github.com>
2025-03-31 13:52:58 +0300

    fixes in _1.py

--- 2 semester/lab5/_1.py
+++ 2 semester/lab5/_1.py
@@ -1,14 +1,21 @@
-def cycle_bin_srch(arr: list, index: int):
-    left = 0
-    right = len(arr)-1
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] == index:
+def cycle_bin_srch(arr: list, x: int, l=0, r=None):
+    if r is None:  # первая обработка значений в самом-самом начале
+        r = len(arr) - 1  # три сценария: либо n > 1, либо n == 1, либо n == 0
+        if not r:
+            if x == arr[r]:
+                return r
+            else:
+                return -1
+        elif r == -1:
+            raise ValueError("Array is empty")
+    while l <= r:
+        mid = (l + r) // 2
+        if arr[mid] == x:
             return mid
-        elif arr[mid] < index:
-            left = mid + 1
+        elif arr[mid] < x:
+            l = mid + 1
         else:
-            right = mid - 1
+            r = mid - 1
     return -1
 
 
