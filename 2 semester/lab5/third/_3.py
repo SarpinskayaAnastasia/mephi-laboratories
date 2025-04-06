ac6348a595dd101129703aed2d168a482e6b4285
SarpinskayaAnastasia <74780246+SarpinskayaAnastasia@users.noreply.github.com>
2025-04-06 17:51:39 +0300

    THIRD TASK IS DONE HOORRAY

--- 2 semester/lab5/third/_3.py
+++ 2 semester/lab5/third/_3.py
@@ -2,8 +2,9 @@ from _2 import bin_paste_sort
 from essentials import HashTable
 
 students_db = HashTable(32)  # {(subject, mark): [students]}
-sub_statistics = HashTable(32)  # {subject: [sum: int, num: int, average: float]}
-subjects = set()
+sub_statistics = HashTable(32)  # {subject: [sum: int, count: int, average: float]}
+subjects = set()  # set[str]
+grades_stats = []  # list[list[name: str, sum: int, count: int, grades: float]]
 
 
 def tuple_to_str(subject: str, mark: int) -> str:
@@ -19,7 +20,7 @@ def add_to_students_db(name: str, subject: str, mark: int):
         students_db.insert_with_key(key, [(name, subject, mark)])
 
 
-def update_sub_stats(name: str, subject: str, mark: int):
+def update_sub_stats(subject: str, mark: int):
     existing = sub_statistics.get_value(subject)
     if existing is not None:
         existing[0] += mark
@@ -29,10 +30,26 @@ def update_sub_stats(name: str, subject: str, mark: int):
         sub_statistics.insert_with_key(subject, [mark, 1, mark])
 
 
+def update_grades(name: str, mark: int):
+    if not grades_stats:
+        grades_stats.append([name, mark, 1, mark])
+    else:
+        target = [name, mark, 1, mark]
+        for t in grades_stats:
+            if t[0] == name:
+                t[1] += target[1]
+                t[2] += target[2]
+                t[3] = t[1] / t[2]
+                return None
+        grades_stats.append(target)
+    bin_paste_sort(grades_stats, key=lambda x: -x[3])
+
+
 def update_all_dbs(name: str, subject: str, mark: int):
     add_to_students_db(name, subject, mark)
-    update_sub_stats(name, subject, mark)
+    update_sub_stats(subject, mark)
     subjects.add(subject)
+    update_grades(name, mark)
 
 
 if __name__ == "__main__":
@@ -61,3 +78,6 @@ if __name__ == "__main__":
     print()
     for sub in subjects:
         print(f"{sub}: {sub_statistics.get_value(sub)[2]:.2f}")
+
+    print()
+    print(*[(i[0], i[3]) for i in grades_stats[:3]], sep='\n')
