d220ded075dfe29fc2c11a1fc0b0dfb90b873c1d
Lizavetick <liza.log2019@bk.ru>
2025-04-07 12:32:24 +0300

    Update _2.py

--- 2 semester/lab5/third/_2.py
+++ 2 semester/lab5/third/_2.py
@@ -8,14 +8,16 @@ def bin_search(arr: list, key_val, l: int, r: int, key=lambda x: x) -> int:
     return l
 
 
+def only_shift(arr: list, current_i: int, key=lambda x: x):
+    current = arr[current_i]
+    key_val = key(current)
+    insert_pos = bin_search(arr, key_val, 0, current_i - 1, key)
+    for j in range(current_i, insert_pos, -1):
+        arr[j] = arr[j - 1]
+    arr[insert_pos] = current
+
+
 def bin_paste_sort(arr: list, key=lambda x: x):
     n = len(arr)
     for i in range(1, n):
-        current = arr[i]
-        key_val = key(current)
-        insert_pos = bin_search(arr, key_val, 0, i - 1, key)
-
-        for j in range(i, insert_pos, -1):
-            arr[j] = arr[j - 1]
-
-        arr[insert_pos] = current
+        only_shift(arr, i, key)
