dbc861c6a8eb1aae1e5720f8ed410ec28b635975
SarpinskayaAnastasia <s04n02@gmail.com>
2025-05-12 13:13:05 +0300

    START

---
+++ 2 semester/lab9-PROJECT/data_struct.py
@@ -0,0 +1,8 @@
+class Book:
+    def __init__(self):
+        self.isbn = ""
+        self.name = ""
+        self.author = ""
+        self.year = 0
+        self.genre = 0
+        self.available = True
